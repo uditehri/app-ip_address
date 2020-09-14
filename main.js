2206ec67e290420f35817de0e1f1250a9f96ca54
uditehri <udit.tehri@centurylink.com>
2020-09-14 16:13:22 +0000

    Lab 3

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
   Assign the class definition to variable IPCIDR.
 */
 const IPCIDR = require('ip-cidr');
+const path = require('path');
 const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
 
 /**
